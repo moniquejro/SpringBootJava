2310444066719a75701205b4e2d406159c2da73a
unknown <moniquejroliveira@gmail.com>
2020-07-27 13:04:56 -0300

    Minha primeira aplicação

---
+++ hello1/src/main/java/com/helloworld1/hello1/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.helloworld1.hello1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello1")
+public class HelloController {
+	
+	@GetMapping
+	public String hello()
+	{
+		return "Hello, Generation!";
+	}
+
+}
