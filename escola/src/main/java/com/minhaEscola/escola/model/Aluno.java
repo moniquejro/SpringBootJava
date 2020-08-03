10cbbff30bb17c406e9cad95d0b92be38a47e8af
unknown <moniquejroliveira@gmail.com>
2020-08-03 08:54:34 -0300

    model aluno

---
+++ escola/src/main/java/com/minhaEscola/escola/model/Aluno.java
@@ -0,0 +1,62 @@
+package com.minhaEscola.escola.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "tb_aluno")
+public class Aluno {
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String nome;
+
+	private boolean matriculado;
+	
+	@ManyToOne
+	@JsonIgnoreProperties ("aluno")
+	private Turma turma;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public boolean isMatriculado() {
+		return matriculado;
+	}
+
+	public void setMatriculado(boolean matriculado) {
+		this.matriculado = matriculado;
+	}
+	
+	public Turma getTurma() {
+		return turma;
+	}
+
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}
+
+}
