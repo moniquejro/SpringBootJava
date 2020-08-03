1a81b5bb46c905e77a26fec97203b75deb68af1b
unknown <moniquejroliveira@gmail.com>
2020-08-03 08:49:39 -0300

    modificação model turma

--- escola/src/main/java/com/minhaEscola/escola/model/Turma.java
+++ escola/src/main/java/com/minhaEscola/escola/model/Turma.java
@@ -1,16 +1,20 @@
 package com.minhaEscola.escola.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity
-@Table (name = "dados")
+@Table (name = "tb_turma")
 public class Turma {
 	
 	@Id
@@ -18,11 +22,15 @@ public class Turma {
 	private long id;
 	
 	@NotNull
-	@Size(min = 5, max =100)
 	private String turma;
 	
-	@NotNull
+	
 	private boolean ativo;
+
+	@OneToMany(mappedBy = "turma", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("turma")
+	private List<Aluno> aluno;
+	
 
 	public long getId() {
 		return id;
@@ -46,6 +54,14 @@ public class Turma {
 
 	public void setAtivo(boolean ativo) {
 		this.ativo = ativo;
+	}
+	
+	public List<Aluno> getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(List<Aluno> aluno) {
+		this.aluno = aluno;
 	}
 
 }
