ab22f8a9272b3980b90df9efe2dabc155db76ff3
unknown <moniquejroliveira@gmail.com>
2020-08-03 08:50:51 -0300

    modificação repository turma

--- escola/src/main/java/com/minhaEscola/escola/repository/TurmaRepository.java
+++ escola/src/main/java/com/minhaEscola/escola/repository/TurmaRepository.java
@@ -9,6 +9,6 @@ import com.minhaEscola.escola.model.Turma;
 
 @Repository
 public interface TurmaRepository extends JpaRepository<Turma, Long> {
-	public List<Turma> findAllByturmaContainingIgnoreCase (String turma);
+	public List<Turma> findAllByTurmaContainingIgnoreCase (String turma);
 
 }
